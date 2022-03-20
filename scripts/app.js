ba2d6fff7e6f1bbc71e93241420fa78842a219a2
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-20 10:39:22 +0700

    learn how to loading data with VueJs

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,14 @@
 const TodosApp = {
   data() {
     return {
+      isLoading: false,
       todos: [],
       enteredTodoText: "",
       editedTodoId: null,
     };
   },
   methods: {
-    saveTodo(event) {
+    async saveTodo(event) {
       event.preventDefault();
 
       if (this.editedTodoId) {
@@ -29,10 +30,35 @@ const TodosApp = {
         this.editedTodoId = null;
       } else {
         // Creating
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3000/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+
+        const responseData = await response.json();
+
         const newTodo = {
           text: this.enteredTodoText,
-          id: new Date().toISOString(),
+          id: responseData.createdTodo.id,
         };
+
         this.todos.push(newTodo);
       }
 
@@ -50,6 +76,27 @@ const TodosApp = {
         return todoItem.id !== todoId;
       });
     },
+  },
+  async created() {
+    let response;
+    this.isLoading = true;
+    try {
+      response = await fetch("http://localhost:3000/todos");
+    } catch (error) {
+      alert("Something went wrong!");
+      this.isLoading = false;
+      return;
+    }
+
+    this.isLoading = false;
+
+    if (!response.ok) {
+      alert("Something went wrong!");
+      return;
+    }
+
+    const responseData = await response.json();
+    this.todos = responseData.todos;
   },
 };
 
